6af002d0879045e40dee802da32a6a7e2c842e9e
3128194922 <3128194922@qq.com>
2025-07-16 21:10:44 +0800

    新增音效，动力剑，泰拉刃等内容

---
+++ Client/kubejs/startup_scripts/key_Noserver.js
@@ -0,0 +1,12 @@
+const $KeyMappingRegistry = Java.loadClass("dev.architectury.registry.client.keymappings.KeyMappingRegistry");
+const $KeyMapping = Java.loadClass("net.minecraft.client.KeyMapping");
+const $GLFWkey = Java.loadClass("org.lwjgl.glfw.GLFW");
+
+ClientEvents.init(() => {
+    global.regKeyCharm = new $KeyMapping(
+        "key.rainbow.charmkey", //按键的组名
+        $GLFWkey.GLFW_KEY_F,
+        "key.keybinding.rainbow.charmkey" //按键的名字
+    );
+  $KeyMappingRegistry.register(global.regKeyCharm);
+});
